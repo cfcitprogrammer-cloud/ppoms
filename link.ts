40fe9a2fb272a3042a267f3bdc7f4b2dc9c1738a
cfcitprogrammer-cloud <cfcitprogrammer@gmail.com>
2026-02-10 08:13:37 +0800

    hotfix

--- link.ts
+++ link.ts
@@ -1,2 +1,2 @@
 export const GAS_DEPLOYMENT_LINK =
-  "https://script.google.com/macros/s/AKfycbxA-7xsDupkxdG14vH0VMilaWHDzHcY1FZCHSVL9_HxpuJy3UMXrJ63Y_MzW01gCXk/exec";
+  "https://script.google.com/macros/s/AKfycbwdgQN_-6aiwY_VYpZusA2hCfXG_ruJrzaj7RErEyNBZTX2tQxNc4caKiv_s7sUAf6R/exec";
